df22571f35284c2b52d3da4583a198fa1605b9b8
Roman Vishnevskii <roman.vishnevsky@xored.com>
2017-06-27 22:40:01 +0700

    Improve counter

---
+++ Vishnevsky.Nsudotnet.LineCounter/Counter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+
+
+namespace LinesCounter
+{
+	public class Counter
+	{
+		private Regex _rowRegExp;
+
+		/// <summary>
+		/// Calculate amount of not empty lines in all files in the specified folder
+		/// </summary>
+		/// <param name="dirPath"></param>
+		/// <param name="fileFilter"> e.g. *.cs </param>
+		/// <param name="rowFilter">Regular expression to exclude line</param>
+		/// <returns></returns>
+		public int GetProjectLines(string folder, string fileFilter, string rowFilter)
+		{
+			DirectoryInfo dir = new DirectoryInfo(folder);
+
+			if (!String.IsNullOrEmpty(rowFilter))
+			{
+				_rowRegExp = new Regex(rowFilter);
+			}
+
+			return dir.EnumerateFiles(fileFilter, SearchOption.AllDirectories).Sum(f => GetFileLines(f.FullName));
+		}
+
+		protected int GetFileLines(string filePath)
+		{
+			return File.ReadLines(filePath).Count(line => IsLineInvolved(line));
+		}
+
+		protected Boolean IsLineInvolved(string line)
+		{
+			if (String.IsNullOrWhiteSpace(line))
+			{
+				return false;
+			}
+
+			if (_rowRegExp != null)
+			{
+				return !_rowRegExp.IsMatch(line);
+			}
+
+			return true;
+		}
+	}
+}
--- Vishnevsky.Nsudotnet.LineCounter/Program.cs
+++ Vishnevsky.Nsudotnet.LineCounter/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LinesCounter;
+using System;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -7,37 +8,6 @@ namespace LineCounter
 {
 	class Program
 	{
-		static Boolean IsLineInvolved(string line, string rowRegExp)
-		{
-			line = line.Trim();
-			if (String.IsNullOrEmpty(line))
-			{
-				return false;
-			}
-
-			if (!String.IsNullOrEmpty(rowRegExp))
-			{
-				Regex regex = new Regex(rowRegExp);
-				return !regex.IsMatch(line);
-			}
-
-			return true;
-		}
-
-		public static int GetFileLines(string filePath, string rowRegExp)
-		{
-			return File.ReadLines(filePath).Where(line => IsLineInvolved(line, rowRegExp)).Count();
-		}
-
-		public static int GetProjectLines(string dirPath, string fileFilter, string rowFilter)
-		{
-			DirectoryInfo dir = new DirectoryInfo(dirPath);
-
-			return dir.EnumerateFiles(fileFilter, SearchOption.AllDirectories)
-				.Sum(f => GetFileLines(f.FullName, rowFilter));
-		}
-
-
 		static void Main(string[] args)
 		{
 			try
@@ -52,14 +22,14 @@ namespace LineCounter
 				}
 				else
 				{
-					int lines = GetProjectLines(options.Directory, options.FileFilter, options.RowFilter);
+					var counter = new Counter();
+					int lines = counter.GetProjectLines(options.Directory, options.FileFilter, options.RowFilter);
 					Console.WriteLine(lines);
 				}
 			}
 			catch (Exception e)
 			{
 				Console.WriteLine(e.Message);
-				return;
 			}
 		}
 	}
